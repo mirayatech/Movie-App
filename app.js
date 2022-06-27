1182f27410ba0b218c7a987856ec3c8dccf6eee0
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-27 13:10:44 +0200

    add img path

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 
 const APIURL = " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1";
 
+const IMGPATH = "https://image.tmdb.org/t/p/w1208"
+
 async function getMovies() {
     const response = await fetch(APIURL)
     const responseData = await response.json();
