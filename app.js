0d5bc64f31032d43add7abf4d4fa997d255899d1
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-27 16:45:07 +0200

    format document

--- app.js
+++ app.js
@@ -2,35 +2,36 @@
 // https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1
 
 const APIURL =
-    " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1";
+  " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1";
 
 const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 async function getMovies() {
-    const response = await fetch(APIURL);
-    const responseData = await response.json();
+  const response = await fetch(APIURL);
+  const responseData = await response.json();
 
-    console.log(responseData);
+  console.log(responseData);
 
-    responseData.results.forEach((movie) => {
+  responseData.results.forEach((movie) => {
+    const { poster_path, title, vote_average } = movie;
 
-        const { poster_path, title, vote_average } = movie
+    const movieElement = document.createElement("div");
+    movieElement.classList.add("movie");
 
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie');
-
-        movieElement.innerHTML = `
-                                <img src="${IMGPATH + poster_path}" alt="${title}">
+    movieElement.innerHTML = `
+                                <img src="${
+                                  IMGPATH + poster_path
+                                }" alt="${title}">
                                 <div class="movie-info">
                                 <h3>${title}</h3>
                                 <span class="rating">${vote_average}</span>
                                 </div>
-                                `
+                                `;
 
-document.querySelector('main').appendChild(movieElement)
-    });
+    document.querySelector("main").appendChild(movieElement);
+  });
 
-    return responseData;
+  return responseData;
 }
 
 getMovies();
