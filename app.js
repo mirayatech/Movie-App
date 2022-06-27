491f7afb7c34ddd236fd8a226465d68e14aa34af
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-27 13:24:46 +0200

    fetching the movie posters to the body

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 // key: cc711dbd2e5433016dae5bfb30562cde
 // https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1
 
-const APIURL = " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1";
+const APIURL =
+  " https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=cc711dbd2e5433016dae5bfb30562cde&page=1";
 
-const IMGPATH = "https://image.tmdb.org/t/p/w1208"
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
 
 async function getMovies() {
-    const response = await fetch(APIURL)
-    const responseData = await response.json();
+  const response = await fetch(APIURL);
+  const responseData = await response.json();
 
-    console.log(responseData);
+  console.log(responseData);
 
-    return responseData
+  responseData.results.forEach((movie) => {
+    const img = document.createElement("img");
+    img.src = IMGPATH + movie.poster_path;
+
+    document.body.appendChild(img);
+  });
+
+  return responseData;
 }
 
-getMovies()
+getMovies();
