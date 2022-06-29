25270ccd93801d26f09eb1e78fa74d7a7c3d4b5c
Miraya Abrodi <mirayaabrodi@gmail.com>
2022-06-29 08:38:43 +0200

    add star to movies

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ const search = document.getElementById("search");
 const container = document.querySelector(".container");
 const navLinks = document.querySelectorAll('.nav-item')
 
+
+
 // initially get fav movies
 getMovies(APIURL);
 
@@ -28,6 +30,91 @@ async function getMovies(url) {
 
 
 
+// show movies
+function showMovies(movies) {
+  main.innerHTML = "";
+
+  // create movie Layout
+  movies.forEach((movie) => {
+    const { poster_path, title, vote_average, release_date, overview } = movie;
+
+    const movieEl = document.createElement("div");
+    movieEl.classList.add("movie");
+
+    movieEl.innerHTML = `
+                  <i onclick="addToFavorite(this)" class="fa-solid fa-star"></i>
+                  <img src="${IMGPATH + poster_path}" alt="${title}"/>
+                  <div class="primary-info">
+                      <h3>${title}</h3>
+                      <div class ="secondary-info">
+                      <p class"date">${convertTime(release_date)}</p>
+                      <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                  </div>
+                  </div>
+              `;
+    main.appendChild(movieEl);
+
+
+
+
+    // show container layout, if we click on movie
+    movieEl.addEventListener("click", () => {
+      container.classList.add("show");
+
+      container.innerHTML = `
+                                  <img src="${IMGPATH + poster_path}" alt="${title}"/>
+                                      <div class="info">
+                                        <h1>${title} <span class="light-text">(${convertYearOnly(release_date)})</span></h1>
+                                      <div class="first-info">
+                                          <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+                                          <i onclick="addToFavorite(this)" class="fa-solid fa-star"></i>
+                                      </div>
+                                      <div class="second-info">
+                                          <h3>Overview</h3>
+                                          <p>${overview}</p>
+                                        </div>
+
+                                      </div>`;
+    });
+
+
+
+  });
+}
+
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    document.querySelector('.nav-item.active').classList.remove('active')
+    link.classList.add('active')
+  })
+})
+
+// search for MOvie
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value;
+
+  if (searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+    search.value = "";
+    container.classList.remove("show");
+  }
+});
+
+
+
+// change color of rating count
+function getClassByRate(vote) {
+  if (vote >= 8) {
+    return "green";
+  } else if (vote >= 5) {
+    return "orange";
+  } else {
+    return "red";
+  }
+}
 // Convert time
 const convertTime = (time) => {
   return new Date(time).toLocaleDateString("en-us", {
@@ -45,89 +132,7 @@ const convertYearOnly = (time) => {
 };
 
 
-// show movies
-function showMovies(movies) {
-  main.innerHTML = "";
+// function addToFavorite(e) {
+//   console.log(e.parentElement)
+// }
 
-  // create movie Layout
-  movies.forEach((movie) => {
-    const { poster_path, title, vote_average, release_date, overview } = movie;
-
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
-
-    movieEl.innerHTML = `
-            <img src="${IMGPATH + poster_path}" alt="${title}"/>
-            <div class="primary-info">
-                <h3>${title}</h3>
-                <div class ="secondary-info">
-                <p class"date">${convertTime(release_date)}</p>
-                <span class="${getClassByRate(
-      vote_average
-    )}">${vote_average}</span>
-            </div>
-            </div>
-        `;
-    main.appendChild(movieEl);
-
-    // show container layout, if we click on movie
-    movieEl.addEventListener("click", () => {
-      container.classList.add("show");
-
-      container.innerHTML = `
-                        <img src="${IMGPATH + poster_path}" alt="${title}"/>
-                            <div class="info">
-                              <h1>${title} <span class="light-text">(${convertYearOnly(
-        release_date
-      )})</span></h1>
-
-
-                            <div class="first-info">
-                                <span class="${getClassByRate(
-        vote_average
-      )}">${vote_average}</span>
-                                <i onClick"updateStatus(this)" class="fa-solid fa-heart"></i>
-                                <i  onClick"updateStatus(this)" class="fa-solid fa-bookmark"></i>
-                            </div>
-                            <div class="second-info">
-                                <h3>Overview</h3>
-                                <p>${overview}</p>
-                              </div>
-
-                            </div>`;
-    });
-  });
-}
-
-// change color of rating count
-function getClassByRate(vote) {
-  if (vote >= 8) {
-    return "green";
-  } else if (vote >= 5) {
-    return "orange";
-  } else {
-    return "red";
-  }
-}
-
-// search for MOvie
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  const searchTerm = search.value;
-
-  if (searchTerm) {
-    getMovies(SEARCHAPI + searchTerm);
-
-    search.value = "";
-
-    container.classList.remove("show");
-  }
-});
-
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    document.querySelector('.nav-item.active').classList.remove('active')
-    link.classList.add('active')
-  })
-})
